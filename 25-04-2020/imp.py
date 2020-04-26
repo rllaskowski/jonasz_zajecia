ab31d3c975de0b76be9d25d67e21979ff2bf3d45
rllaskowski <robert.laskowski0@gmail.com>
2020-04-26 15:48:01 +0200

    update

---
+++ 25-04-2020/imp.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+"""
+*przyklad*
+
+*ania wchodzi*
+
+
+*wchodza *
+Asia Wojtek Grzegorz
+
+"""
+
+
+def czyChlopiec(imie):
+    if imie[-1] == 'a':
+        return False
+    else:
+        return True
+
+def main():
+    x = input().split()
+    n = int(x[0])
+    p = int(x[1])
+
+    kolejka = []
+
+    for i in range(n):
+        imie = input()
+        print("wchodzi", imie)
+        
+        if czyChlopiec(imie) == False:
+            kolejka.append(imie)
+        else:
+            kolejka.append(imie)
+            if len(kolejka) > p:
+                
+                while True == True:
+                    imieWychodzacego = kolejka.pop(0)
+                   
+                    print("wychodzi", imieWychodzacego)
+                    if czyChlopiec(imieWychodzacego) == True:
+                       
+                        break
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
