7384572bd4d77f0063136da99e162decca965894
rllaskowski <robert.laskowski0@gmail.com>
2020-04-27 15:35:39 +0200

    update

--- 26-04-2020/kinoman.py
+++ 26-04-2020/kinoman.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+
+"""
+
+6 2 4 3 2 5 1
+
+
+"""
+
+
+def main():
+    n = int(input())
+
+    wieze = input().split()
+
+    for i in range(n):
+        wieze[i] = int(wieze[i])
+        
+    for i in range(n):
+        k = i-1
+        while k >= 0 and wieze[k] < wieze[i]:
+            k -= 1
+        
+        if k == -1:
+            print("-1", end=" ")
+        else:
+            print(k+1, end=" ")
+        
+main()
