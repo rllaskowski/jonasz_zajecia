831163f5706c0e987f2a4cf66e0dccdeba9eb13e
rllaskowski <robert.laskowski0@gmail.com>
2020-05-16 16:31:54 +0200

    update

--- 16-05-2020/karty.py
+++ 16-05-2020/karty.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+
+def main():
+    pieniadze = 0
+    karty = 0
+    n = int(input())
+    x = input().split()
+    c = input()
+
+    for i in range(n):
+        x[i] = int(x[i])
+        if c[i] == '1':
+            pieniadze += x[i]
+
+    x.sort()
+
+    while pieniadze > 0:
+        if pieniadze >= x[karty]:
+            pieniadze -= x[karty]
+            karty += 1
+        else:
+            break
+    print(karty)
+
+main()
