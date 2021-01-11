10f347060381547a869ea248ccc9f82d16e1af58
Robert Laskowski <robert.laskowski0@gmail.com>
2021-01-11 16:36:03 +0100

    update

--- 2021/01/06/kaledarze.cpp
+++ 2021/01/06/kaledarze.cpp
@@ -7,6 +7,38 @@ int B[1000001];
 int ilePA[1000001]; //ile dni przed danym miesiacem
 int ilePB[1000001];
 
+
+int wyszukaj(int ktoryDzien, int pref[], int ileMiesiecy) {
+    /*
+    pref[m] to ilosc dni przed miesiacem m
+    */
+    
+    int pocz = 1;
+    int kon = ileMiesiecy;
+    int s;
+
+
+
+    while (kon >= pocz) {
+        s = (pocz+kon) / 2;
+
+        // s to numer jakiegos miesiaca
+        // teraz chcemy sprawdzic czy ten miesiac
+        // jest za wczesnie
+        // za pozno
+        // czy moze to dobry miesiac
+        if (pref[s] < ktoryDzien && pref[s+1] >= ktoryDzien) {
+            return s;
+        } else if (pref[s] < ktoryDzien) {
+            pocz = s+1;
+        } else {
+            kon = s-1;
+        }
+    }
+
+}
+
+
 int main() {
     int n, m;
     int z;
@@ -24,6 +56,9 @@ int main() {
         ilePB[i] = ilePB[i-1] + B[i-1];
     }
 
+    ilePA[n+1] = ilePA[n] + A[n];
+    ilePB[m+1] = ilePB[m] + B[m];
+
     cin >> z;
 
     for (int i = 0; i < z; i++) {
@@ -33,7 +68,7 @@ int main() {
         int ileDni = D; // ktory dzien w roku
         int *kal = A; // tablica
         int *kal2 = B;
-        int M2 = 1;
+        int M2;
         if (C == 'B') {
            swap(kal, kal2);
         }
@@ -44,12 +79,23 @@ int main() {
             ileDni += ilePB[M];
         }
 
-        while (dni + kal2[M2] < ileDni) {
-            dni += kal2[M2];
-            M2++;
+        
+        if (C == 'A') {
+            M2 = wyszukaj(ileDni, ilePB, m);
+            dni = ileDni - ilePB[M2];
+        } else {
+            M2 = wyszukaj(ileDni, ilePA, n);
+            dni = ileDni - ilePA[M2];
         }
        
-        cout << ileDni - dni << " " << M2 << endl;
+       
+       
+        /*while (dni + kal2[M2] < ileDni) {
+            dni += kal2[M2];
+            M2++;
+        }*/
+       
+        cout << dni << " " << M2 << endl;
     }
 
     
